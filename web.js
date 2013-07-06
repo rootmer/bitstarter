718207c42bd69e6e063ccf4fb08f1117638cd70a
Toni Turpeinen <rootmer@gmail.com>
2013-07-06 13:13:27 +0000

    modified web.js

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.redFileSync(index.html)));
+  response.send(buf.toString(fs.readFileSync(index.html)));
 });
 
 var port = process.env.PORT || 5000;
